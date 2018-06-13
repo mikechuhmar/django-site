a11b38a84ec73ca7b5bb5542c0389f75bd917da4
mikechuhmar <miha1797@gmail.com>
2018-06-14 01:15:11 +0300

    models

--- djangoshop/ecomapp/admin.py
+++ djangoshop/ecomapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from ecomapp.models import Category, Brand, Product
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('name', )}
+# Модель товара
+class ProductAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('name', )}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Brand)
+'''admin.site.register(Category)
+
+admin.site.register(Product)'''
--- djangoshop/ecomapp/models.py
+++ djangoshop/ecomapp/models.py
@@ -1,3 +1,45 @@
+from __future__ import unicode_literals
 from django.db import models
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
+from transliterate import translit
 
-# Create your models here.
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+def pre_save_category_slug(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        #slug = slugify(translit(unicode_literals(instance.name), reversed=True))
+        slug = slugify(translit(instance.name, reversed=True))
+        instance.slug = slug
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+def image_folder(instance, filename):
+    filename = instance.slug + '.' + filename.split('.')[1]
+    return "{0}/{1}".format(instance.slug, filename)
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+
+    description = models.TextField()
+    image = models.ImageField(upload_to=image_folder)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
