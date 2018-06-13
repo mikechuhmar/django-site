ead8b26db4933e6e26f4a0cb94b4c06e21786f66
mikechuhmar <miha1797@gmail.com>
2018-06-13 16:57:11 +0300

    djangoshop: Add static

--- djangoshop/djangoshop/settings.py
+++ djangoshop/djangoshop/settings.py
@@ -124,4 +124,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIR = os.path.join(BASE_DIR, 'static_in_dev')
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static_in_dev') ]
--- djangoshop/djangoshop/urls.py
+++ djangoshop/djangoshop/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from ecomapp.views import hello_world
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hello_world, name='base'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
